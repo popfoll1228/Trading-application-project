7425539674f10d2d7d2735b9a7d99586c6bc409f
Joe Wang <wangchengxu95@gmail.com>
2025-12-23 16:46:33 +0800

    Reading data

--- trading.py
+++ trading.py
@@ -1,6 +1,9 @@
+#1_Import
 import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
 
-def read(txtname):
-    return
+#2_Read data [Day,Time,Open,High,Low,Close]
+txtname = input('Enter name of the data file: ')
+data =pd.read_excel(txtname)   #USERINPUT
+datalist = data.values.tolist()
